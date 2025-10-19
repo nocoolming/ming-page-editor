d72e5a98a4ff5cd60296d1f56003ddc6d184d176
Mingming Li <myliming@gmail.com>
2025-10-19 15:55:36 +0800

    不知道改了什么

--- apps/demos/demo-react-router/blogConfigs.tsx
+++ apps/demos/demo-react-router/blogConfigs.tsx
@@ -1,36 +1,71 @@
 import type { Config, } from "@nocoolming/page-editor";
 
-export const blockConfigs: Record<string, BlockConfig> = {
-    Text: {
-        fields: [{
-            key: 'text',
-            label: '文本',
-            type: 'string'
-        }],
-        acceptsChildren: false,
-        render: (props) => {
-            console.log(JSON.stringify(props));
-            return (
-                <p>{props.text}</p>
+export const config = {
+    components: {
+        HeadingBlock: {
+            fields: {
+                title: {
+                    type: 'text',
+                }
+            },
+            render: ({ title }: { title: string }) => {
+                return (
+                    <h1>${title}</h1>
+                )
+            }
+        },
+        ImageBlock: {
+            fields: {
+                src: {
+                    type: 'string',
+                },
+                alt: {
+                    type: "string",
+                }
+            },
+            render: ({ alt, src }: { alt: string, src: string }) => (
+                <img src={src} alt={alt} />
             )
+        },
+        ContainerBlock:{
+            render: ( )=> {
+                <div></div>
+            }
         }
-    },
-    Image: {
-        fields: [{
-            key: 'src',
-            label: 'Image url',
-            type: 'string'
-        }],
-        acceptsChildren: false,
-        render: props => <img src={props.src} alt='' className="max-w-xs block" />
-    },
-    Container: {
-        fields: [],
-        acceptsChildren: true,
-        render: (_, children) => (
-            <div className="">
-                {children}
-            </div>
-        )
     }
-};
+}
+
+// export const blockConfigs: Record<string, BlockConfig> = {
+//     Text: {
+//         fields: [{
+//             key: 'text',
+//             label: '文本',
+//             type: 'string'
+//         }],
+//         acceptsChildren: false,
+//         render: (props) => {
+//             console.log(JSON.stringify(props));
+//             return (
+//                 <p>{props.text}</p>
+//             )
+//         }
+//     },
+//     Image: {
+//         fields: [{
+//             key: 'src',
+//             label: 'Image url',
+//             type: 'string'
+//         }],
+//         acceptsChildren: false,
+//         render: props => <img src={props.src} alt='' className="max-w-xs block" />
+//     },
+//     Container: {
+//         fields: [],
+//         acceptsChildren: true,
+//         render: (_, children) => (
+//             <div className="">
+//                 {children}
+//             </div>
+//         )
+//     }
+// };
